1fdbd258d620f88d830621473eb8a4d7001c9753
tlr24 <tlr24@njit.edu>
2021-10-18 08:46:01 -0700

    Backend consume.py updated to register once

--- Backend/consume.py
+++ Backend/consume.py
@@ -4,6 +4,20 @@ import json
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('172.26.169.103', 5672, '/', pika.PlainCredentials('admin', 'Luftwaffe1')))
 channel = connection.channel()
+connection2 = pika.BlockingConnection(pika.ConnectionParameters('172.26.169.103', 5672, '/', pika.PlainCredentials('admin', 'Luftwaffe1')))
+channel2 = connection2.channel()
+def send_frontend(msg):
+	channel2.basic_publish(exchange='amq.direct',routing_key='c#TOfe',body=msg)
+	print("sent to frontend")
+
+def callback_frontend(ch, method, properties, body):
+	print(f'{body} is received')
+	body = str(body)
+	body = body[1:]
+	body = body.replace("'", "")
+	print(body)
+	send_frontend(body)
+
 def send(fmsg):
 	json_msg = json.loads(fmsg)
 	print(json_msg["email"] + " " + json_msg["password"])
@@ -17,11 +31,13 @@ def send(fmsg):
 		query = f"INSERT INTO public.users(f_name, l_name, email, password, ins_id) VALUES ('{firstName}', '{lastName}', '{email}', '{password}', 1)"
 	else:
 		query = f"SELECT * FROM public.users WHERE email={email} AND password={password}"
+		
 	channel.basic_publish(exchange='amq.direct',routing_key='c#TOdb',body=query)
-	print(query)
-	stream = open("send.py")
-	read_file = stream.read()
-	exec("send")
+	print("Sent: " + query)
+	#exec(open("send.py").read())
+	channel2.basic_consume(queue="postgresqlTOc#backend", on_message_callback=callback_frontend, auto_ack=True)
+	channel2.start_consuming()
+	
 
 
 def callback(ch, method, properties, body):
--- Backend/send.py
+++ Backend/send.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 import pika 
 
+print("reached send.py")
 connection = pika.BlockingConnection(pika.ConnectionParameters('172.26.169.103', 5672, '/', pika.PlainCredentials('admin', 'Luftwaffe1')))
 channel = connection.channel()
 
-def send(msg):
+def send_frontend(msg):
 	channel.basic_publish(exchange='amq.direct',routing_key='c#TOfe',body=msg)
 	print("sent to frontend")
 	connection.close()
 
-def callback(ch, method, properties, body):
+def callback_frontend(ch, method, properties, body):
 	print(f'{body} is received')
-	send(body)
+	body = str(body)
+	body = body[1:]
+	body = body.replace("'", "")
+	print(body)
+	send_frontend(body)
 
-channel.basic_consume(queue="postgresqlTOc#backend", on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue="postgresqlTOc#backend", on_message_callback=callback_frontend, auto_ack=True)
 
 channel.start_consuming()
