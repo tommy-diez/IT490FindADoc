44294f5fc88a7e15a31a8b1b0989946edb99749d
tlr24 <tlr24@njit.edu>
2021-11-14 14:48:47 -0800

    Updated consume.py w/ new rabbitmq IP & user

--- Backend/consume.py
+++ Backend/consume.py
@@ -2,9 +2,9 @@
 import pika
 import json
 
-con_consume = pika.BlockingConnection(pika.ConnectionParameters('172.26.169.103', 5672, '/', pika.PlainCredentials('admin', 'Luftwaffe1')))
+con_consume = pika.BlockingConnection(pika.ConnectionParameters('172.26.115.206', 5672, '/', pika.PlainCredentials('findadoc', 'Findadoc!')))
 channel_consume = con_consume.channel()
-con_publish = pika.BlockingConnection(pika.ConnectionParameters('172.26.169.103', 5672, '/', pika.PlainCredentials('admin', 'Luftwaffe1')))
+con_publish = pika.BlockingConnection(pika.ConnectionParameters('172.26.115.206', 5672, '/', pika.PlainCredentials('findadoc', 'Findadoc!')))
 channel_publish = con_publish.channel()
 
 def consume_db():
@@ -32,9 +32,9 @@ def send(fmsg):
 	if json_msg.keys() >= {"firstName","lastName"}:
 		firstName = json_msg["firstName"]
 		lastName = json_msg["lastName"]
-		query = "INSERT INTO public.users(f_name, l_name, email, password, ins_id) VALUES (%(firstName)s, %(lastName)s, %(email)s, %(password)s, 1), {'firstName': firstName, 'lastName': lastName, 'email': email, 'password': password}"
+		query = '''"INSERT INTO public.users(f_name, l_name, email, password, ins_id) VALUES (%(firstName)s, %(lastName)s, %(email)s, %(password)s, 1)", {'firstName': firstName, 'lastName': lastName, 'email': email, 'password': password}'''
 	else:
-		query = "SELECT f_name, l_name FROM public.users WHERE email=%(email)s AND password=%(password)s, {'email': email, 'password': password}"
+		query = '''"SELECT f_name, l_name FROM public.users WHERE email=%(email)s AND password=%(password)s", {'email': email, 'password': password}'''
 		
 	channel_publish.basic_publish(exchange='amq.direct',routing_key='c#TOdb',body=query)
 	print("Sent: " + query)
