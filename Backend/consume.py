0a5bd2b08efce081b21cfb3d725e7fb84f350dc3
tlr24 <tlr24@njit.edu>
2021-11-15 08:44:56 -0800

    Updated consume.py with new queues/exchanges and reverted queries

--- Backend/consume.py
+++ Backend/consume.py
@@ -2,20 +2,20 @@
 import pika
 import json
 
-con_consume = pika.BlockingConnection(pika.ConnectionParameters('172.26.115.206', 5672, '/', pika.PlainCredentials('findadoc', 'Findadoc!')))
+con_consume = pika.BlockingConnection(pika.ConnectionParameters('172.26.30.225', 5672, '/', pika.PlainCredentials('findadoc', 'Findadoc!')))
 channel_consume = con_consume.channel()
-con_publish = pika.BlockingConnection(pika.ConnectionParameters('172.26.115.206', 5672, '/', pika.PlainCredentials('findadoc', 'Findadoc!')))
+con_publish = pika.BlockingConnection(pika.ConnectionParameters('172.26.30.225', 5672, '/', pika.PlainCredentials('findadoc', 'Findadoc!')))
 channel_publish = con_publish.channel()
 
 def consume_db():
-	channel_consume.basic_consume(queue="postgresqlTOc#backend", on_message_callback=callback_frontend, auto_ack=True)
+	channel_consume.basic_consume(queue="dbTObe", on_message_callback=callback_frontend, auto_ack=True)
 
 def send_frontend(msg):
-	channel_publish.basic_publish(exchange='amq.direct',routing_key='c#TOfe',body=msg)
+	channel_publish.basic_publish(exchange='amq.direct',routing_key='beTOfeRK',body=msg)
 	print("sent to frontend")
 
 def callback_frontend(ch, method, properties, body):
-	print(f'{body} is received')
+	print(f'{body} is received from db')
 	body = str(body)
 	body = body[1:]
 	body = body.replace("'", "")
@@ -32,20 +32,21 @@ def send(fmsg):
 	if json_msg.keys() >= {"firstName","lastName"}:
 		firstName = json_msg["firstName"]
 		lastName = json_msg["lastName"]
-		query = '''"INSERT INTO public.users(f_name, l_name, email, password, ins_id) VALUES (%(firstName)s, %(lastName)s, %(email)s, %(password)s, 1)", {'firstName': firstName, 'lastName': lastName, 'email': email, 'password': password}'''
+		insuranceId = json_msg["insuranceId"]
+		query = f"INSERT INTO public.users(f_name, l_name, email, password, ins_id) VALUES ('{firstName}', '{lastName}', '{email}', '{password}', '{insuranceId}')"
 	else:
-		query = '''"SELECT f_name, l_name FROM public.users WHERE email=%(email)s AND password=%(password)s", {'email': email, 'password': password}'''
+		query = f"SELECT f_name, l_name FROM public.users WHERE email='{email}' AND password='{password}'"
 		
-	channel_publish.basic_publish(exchange='amq.direct',routing_key='c#TOdb',body=query)
-	print("Sent: " + query)
+	channel_publish.basic_publish(exchange='amq.direct',routing_key='beTOdbRK',body=query)
+	print("Sent to db: " + query)
 	#exec(open("send.py").read())
 	consume_db()
 
 
 def callback(ch, method, properties, body):
-	print(f'{body} is received')
+	print(f'{body} is received from fe')
 	send(body)
 
-channel_consume.basic_consume(queue="nodejsTOc#backend", on_message_callback=callback, auto_ack=True)
+channel_consume.basic_consume(queue="feTObe", on_message_callback=callback, auto_ack=True)
 
 channel_consume.start_consuming()
