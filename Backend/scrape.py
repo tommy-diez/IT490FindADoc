acd4630d26abda6685dc48c09a3c0c7e7fff05f4
tlr24 <tlr24@njit.edu>
2021-12-05 15:12:23 -0800

    Updated scrape.py to format everything correctly

--- Backend/scrape.py
+++ Backend/scrape.py
@@ -2,15 +2,22 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import re
+
+global states
+states = ['IA', 'KS', 'UT', 'VA', 'NC', 'NE', 'SD', 'AL', 'ID', 'FM', 'DE', 'AK', 'CT', 'PR', 'NM', 'MS', 'PW', 'CO', 'NJ', 'FL', 'MN', 'VI', 'NV', 'AZ', 'WI', 'ND', 'PA', 'OK', 'KY', 'RI', 'NH', 'MO', 'ME', 'VT', 'GA', 'GU', 'AS', 'NY', 'CA', 'HI', 'IL', 'TN', 'MA', 'OH', 'MD', 'MI', 'WY', 'WA', 'OR', 'MH', 'SC', 'IN', 'LA', 'MP', 'DC', 'MT', 'AR', 'WV', 'TX']
 
 def save_html(content, path):
 	with open(path, mode='w', encoding='utf-8') as file:
 		file.write(content)
 
-url = 'https://www.horizonnjhealth.com/findadoctor/doctor/plan-horizon-nj-health/address-Newark,%20NJ%2007102,%20USA/specialty-All%20PCPs/sort-by-distance/sort-order-asc'
-#'https://connect.werally.com/search/providers/07102/page-1?coverageType=medical&lat=40.74&long=-74.17&pf=t&propFlow=true&sort=distance&specialty=98'
-#'https://doctorfinder.horizonblue.com/dhf_search/doctors/plan-garden+state+plan/distance-5/city-newark/state-nj/zip-07102/specialty-all+pcps/page-1/sub-category-all/criteria-all+primary+care+physicians+(pcps)/network-codes-oex1?application=dhf'
-#'https://www.aetnabetterhealth.com/newjersey/find-provider?provsearch=true&name=&MedicalName=&city=&zip=07102&distance=25&county=&plan=&language=&specialty=Family+Practice%2cGeneral+Practice%2cMEDICINE%2cMULTI+PROVIDER+GROUP%2c&access=0&newpatient=0&affiliation=&groupaffiliation=&gender=&agesserved=&certified=0&Sorting=3&NumberofProviders=5&extendedHours=&accreditation=&extendedOfficesHours=&telemedicine=False&page=1'
+url = 'https://www.horizonnjhealth.com/findadoctor/doctor/plan-horizon-nj-health/address-Newark,%20NJ%2007102,%20USA/specialty-Pediatrics/sort-by-distance/sort-order-asc/page-6'
+specialty = 'Pediatrics'
+insurance = 'Horizon'
+policy = 'Horizon NJ Health'
+
+#'https://www.horizonnjhealth.com/findadoctor/doctor/plan-horizon-nj-health/address-Newark,%20NJ%2007102,%20USA/specialty-All%20PCPs/sort-by-distance/sort-order-asc'
+
 
 # Making a GET request
 r = requests.get(url)
@@ -29,43 +36,131 @@ officeNames = soup.select('div.search-results > div > div > address > strong')
 #specialties = soup.select('div.search-results > div > div > div > ul > li')
 phoneNumbers = soup.select('div.search-results > div > div > address > div > span')
 addresses = soup.select('div.search-results > div > div > address')
+firstNames = []
+lastNames = []
 addressList = []
+cities = []
+states = []
+zip_codes = []
 for e in range(len(addresses)):
+	current_name = names[e].string.split(",")[0].split(" ")
+	firstNames.append(current_name[0])
+	if len(current_name) == 3:
+		lastNames.append(current_name[2])
+	else:
+		lastNames.append(current_name[1])
 	addresses[e] = addresses[e].getText().replace('\t', '').split('\n')
-	innerAddressList = []
 	for j in range(len(addresses[e])):
 		#print(str(j) + " " + addresses[e][j])
 		if (j == 0 or j == 1 or j == 3):
 			continue;
-		elif (len(innerAddressList) == 2):
-			addressList.append(innerAddressList);
-			break;
-		else:
-			innerAddressList.append(addresses[e][j])
-'''		
-print(len(specialties))
-for s in range(len(specialties)):
-	#print(specialties[s].string)
-	print("TEST #" + str(s) + ": " + str(specialties[s].string))
-	#if (str(specialties[s].string) == 'None'):
-		#temp_list = soup.select_one('div.search-results:nth-child(' + str(s) + ') > div > div > div > ul > li > a')
-		#print(type(temp_list))
-		#specialties[s] = temp_list.string
-		#print(temp_list)
-'''
+		elif (j == 2):
+			addressList.append(addresses[e][j])
+		elif (j == 4):
+			values = addresses[e][j].split(", ")
+			cities.append(values[0])
+			#regex = re.compile(r'\b(' + '|'.join(states) + r')\b')
+			#states.append(regex.match(values[1]).group())
+			state = values[1].split(" ")[0]
+			states.append(state)
+			zip_codes.append(values[1].replace(state, "").replace(" ", ""))
+			
 			
 #print(addressList)
 			
 print(len(addressList))
 for i in range(len(names)):
 	print("\nPerson " + str(i+1))
-	print(names[i].string)
+	print(firstNames[i])
+	print(lastNames[i])
 	print(phoneNumbers[i].string)
+	print(officeNames[i].string)
 	print(addressList[i])
-	#print(specialties[i].string)
+	print(specialty)
+	print(cities[i])
+	print(states[i])
+	print(zip_codes[i])
 
 
 save_html(soup.prettify(), 'page.txt')
 # file = open('page.txt', mode='w', encoding='utf-8')
 # file.write(soup.prettify())
 
+'''
+while 1:
+	ip_list = ['172.26.169.103', '172.26.30.225', '172.26.83.6']
+	for ip in ip_list:
+		p = subprocess.check_output(["ping", "-c", "1", ip])
+		print(ip)
+		if p:
+			con_consume = pika.BlockingConnection(pika.ConnectionParameters(ip, 5672, '/', pika.PlainCredentials('findadoc', 'Findadoc!')))
+			channel_consume = con_consume.channel()
+			con_publish = pika.BlockingConnection(pika.ConnectionParameters(ip, 5672, '/', pika.PlainCredentials('findadoc', 'Findadoc!')))
+			channel_publish = con_publish.channel()
+
+			def consume_db():
+				channel_consume.basic_consume(queue="dbTObe", on_message_callback=callback_frontend, auto_ack=True)
+
+			def send_frontend(msg):
+				channel_publish.basic_publish(exchange='amq.direct',routing_key='beTOfeRK',body=msg)
+				print("sent to frontend")
+
+			def callback_frontend(ch, method, properties, body):
+				print(f'{body} is received from db')
+				body = str(body)
+				body = body[1:]
+				body = body.replace("'", "")
+				print(body)
+				
+				# need parsing of db message to send true/false to frontend for success for login/register
+				
+				send_frontend(body)
+
+			def send(fmsg):
+				json_msg = json.loads(fmsg)
+				# print(json_msg["email"] + " " + json_msg["password"])
+				
+				i = json_msg["case"]
+				
+				switch(i) {
+					case 1:
+						email = json_msg["email"]
+						password = json_msg["password"]
+						firstName = json_msg["firstName"]
+						lastName = json_msg["lastName"]
+						insuranceId = json_msg["insuranceId"]
+						query = f"INSERT INTO public.users(f_name, l_name, email, password, ins_id) VALUES ('{firstName}', '{lastName}', '{email}', '{password}', '{insuranceId}')"
+						break;
+					case 2:
+						email = json_msg["email"]
+						password = json_msg["password"]
+						query = f"SELECT f_name, l_name FROM public.users WHERE email='{email}' AND password='{password}'"
+						break;
+					case 4:
+						officeId = json_msg['officeId']
+						query = f"SELECT * FROM doctor WHERE off_id='{officeId}'";
+						break;
+					case 5:
+						updatePassword = json_msg["password"]
+						confirmPassword = json_msg["confirmPassword"]
+						updateIns = json_msg["insuranceId"]
+						updateEmail = json_msg["email"]
+						if (updatePassword==confirmPassword):
+							newPassword = updatePassword
+						query = f"UPDATE public.users SET email='{updateEmail}', password='{newPassword}', ins_id='{updateIns}' WHERE email='session variable sent from tom'";
+						break;
+										
+				channel_publish.basic_publish(exchange='amq.direct',routing_key='beTOdbRK',body=query)
+				print("Sent to db: " + query)
+				#exec(open("send.py").read())
+				consume_db()
+
+
+			def callback(ch, method, properties, body):
+				print(f'{body} is received from fe')
+				send(body)
+
+			channel_consume.basic_consume(queue="feTObe", on_message_callback=callback, auto_ack=True)
+
+			channel_consume.start_consuming()
+'''
